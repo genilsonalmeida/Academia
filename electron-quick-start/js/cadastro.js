4b4659a24ab6d3511ed516db88bff281036404ef
Genilson Almeida <genilsonalmeidajr@gmail.com>
2018-08-19 03:16:11 -0300

    Fiz uma pequena limpeza nos códigos. Adicionei o botão finalizar cadastro. Retirei cadastro da navbar.

--- electron-quick-start/js/cadastro.js
+++ electron-quick-start/js/cadastro.js
@@ -24,7 +24,6 @@ $(function(){
     var r = JSON.parse(tbRobos[indice_selecionado]);
     $("#inputRobo").val(r.Nome);
     $("#inputEquipe").val(r.Equipe);
-    //$("#inputRobo").attr("readonly","readonly");
     $("#inputRobo").focus();
     });
     
@@ -90,24 +89,11 @@ function listar(tbRobos){
     }
 }
 
-/*function save(){
-    nameEquipe = document.getElementById('inputEquipe').value.toString();
-    nameRobo = document.getElementById('inputRobo').value.toString();
-    
-    localStorage.setItem(nameRobo,nameEquipe);
-
-    document.getElementById('inputRobo').value = "";
-    document.getElementById('inputEquipe').value = "";
-
-    criarElemento();
+function finalizar()
+{
+var r=confirm("Deseja mesmo finalizar o cadastro?");
+if (r==true)
+  {
+  location.href="./tabela.html";
+  }
 }
-
-function criarElemento() {
-    corpo.innerHTML = [
-        '<tr>' +
-        '<td>' + nameRobo + '</td>' +
-        '<td>' + nameEquipe + '</td>' +
-        '<td><a href="#">Editar</a></td>' +
-        '<td><a href="#">Excluir</a></td>' 
-    ];
-}*/
